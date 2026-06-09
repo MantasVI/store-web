ede15caa4f317feecb24e956b0c820badea49c70
LULE12334 <mantasvisakavicius@gmail.com>
2026-06-09 16:45:22 +0300

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,1 @@
 import './bootstrap';
-export default x ({
-    plugins:[laravel({
-        input: [
-            'resources/css/app.css',
-             'resources/js/app.js',
-              'resources/js/iphone.js',
-               'resources/js/mac.js',
-               'resources/js/add.js',
-        ],
-        refesh: true,
-
-    })],
-
-
-});
